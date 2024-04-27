7f292a32916f6743a4f2c88363f268cdc6d92d52
Manuel Gallud Peñalver <mgallud@aidacanarias.com>
2024-04-27 12:09:26 +0200

    ✅ roll_one_and_not_knocks_down_all_pins

---
+++ BowlingGame/BowlingGame.Test/GameTest.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace BowlingGame.Test;
+
+public class GameTest
+{
+    private Game _game;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _game = new Game();
+    }
+
+    [Test]
+    public void roll_one_and_not_knocks_down_all_pins()
+    {
+        _game.Roll(7);
+
+        _game.Score().Should().Be(7);
+    }
+}
---
+++ BowlingGame/BowlingGame/Game.cs
@@ -0,0 +1,16 @@
+﻿namespace BowlingGame;
+
+public class Game
+{
+    private int _totalPoints;
+
+    public void Roll(int pinsKnocked)
+    {
+        _totalPoints += pinsKnocked;
+    }
+
+    public int Score()
+    {
+        return _totalPoints;
+    }
+}
